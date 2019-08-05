3545fd9fa74d6b7640d6810a5abfaef1725132f8
Ericka Sylvester <sylvester.ericka@gmail.com>
2019-08-05 13:11:40 -0400

    Added pwd, ls and cat

--- bash.js
+++ bash.js
@@ -1,11 +1,17 @@
+
+const {pwd} = require('./pwd');
+const {ls} = require('./ls');
+const {cat} = require('./cat');
+
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', data => {
+  
   const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
-    process.stdout.write(__dirname);
-  }
 
-  process.stdout.write('Your input: ' + cmd);
+  pwd(cmd);
+  ls(cmd);
+  cat(cmd);
   process.stdout.write('\nprompt > ');
 });
